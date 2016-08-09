bcb669bc69cf4225404f654a18804d4c77c4d19a
ICodeMyOwnLife <icodemyownlife@gmail.com>
2016-08-09 16:49:21 +0700

    add login

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
 ﻿var express = require("express");
+var bodyParser = require("body-parser");
 var app = express();
 var server = require("http").Server(app);
 var io = require("socket.io")(server);
+app.use(bodyParser());
 app.use(express.static(__dirname + "/public"));
 
-app.get("/", function (req, res) {
-    res.render("/index.html");
+app.get("/",
+    function (req, res) {
+    res.render("/login.html");
+});
+
+app.post("/login",
+    function (req, res) {
+    var data = req.body;
+    var name = data.name;
+    var team = data.team;
 });
 
 var playerCount = 0;
@@ -20,6 +30,7 @@ function guid() {
     // after the decimal.
     return "_" + Math.random().toString(36).substr(2, 9);
 };
+
 function rtema() {
     // Math.random should be unique because of its seeding algorithm.
     // Convert it to base 36 (numbers + letters), and grab the first 9 characters
@@ -28,7 +39,9 @@ function rtema() {
     if (a <= 1.5) return 1;
     else return 2;
 };
-io.on("connection", function (socket) {
+
+io.on("connection",
+    function (socket) {
     playerCount++;
     id = guid();
     team = rtema();
@@ -42,65 +55,70 @@ io.on("connection", function (socket) {
         //}
         socket.emit("connected", { playerId: id, teamID: team });
         io.emit("count", { playerCount: playerCount });
-    }, 1000);
-
-    socket.on("disconnect", function () {
+    },
+            1000);
+    
+    socket.on("disconnect",
+            function () {
         playerCount--;
         io.emit("count", { playerCount: playerCount });
         console.log("disconnected");
     });
-
-
+    
+    
     // viết một hàm cập nhật điểm ở server
-    socket.on("kdcounter", function (data) {
+    socket.on("kdcounter",
+            function (data) {
         //var i = players.map(ind => ind.pid).indexOf(data['playerId']);
-        var i = players.map(function (playerid) { return playerid.pid}).indexOf(data["playerId"]);
-        if (i>=0) {
-        players[i].k += 1;
-        //console.log('>>'+players[i].k);
+        var i = players.map(function (playerid) { return playerid.pid }).indexOf(data["playerId"]);
+        if (i >= 0) {
+            players[i].k += 1;
+            //console.log('>>'+players[i].k);
             try {
-                socket.broadcast.emit("C_kdcouter", { kill:players[i].k });
-            }
-            catch (e) {
+                socket.broadcast.emit("C_kdcouter", { kill: players[i].k });
+            } catch (e) {
                 console.log("" + e);
             }
         }
     });
     //***********************************  
-    socket.on("update", function (data) {
+    socket.on("update",
+            function (data) {
         try {
             socket.broadcast.emit("updated", data);
-        }
-        catch (e) {
+        } catch (e) {
             console.log("" + e);
         }
     });
-
-    socket.on("wol", function (data) {
+    
+    socket.on("wol",
+            function (data) {
         try {
             if (data[" towerteam"] != data["colteam"])
                 winorlose = data["colteam"];
             else winorlose = 3;
-            socket.broadcast.emit("woled",{result: winorlose });
-        }
-        catch (e) {
+            socket.broadcast.emit("woled", { result: winorlose });
+        } catch (e) {
             console.log("" + e);
         }
     });
-
-    socket.on("tag", function (data) {
+    
+    socket.on("tag",
+            function (data) {
         io.emit("tagged", data);
     });
-
-    socket.on("firex", function (data) {
+    
+    socket.on("firex",
+            function (data) {
         socket.broadcast.emit("fired", data);
 
     });
 });
 
 setInterval(function () {
-    //tagged = false;
-}, 3000);
+        //tagged = false;
+},
+    3000);
 
 var port = 8080;
 
